60700a3fd6ba4ff4043478150f1cd8e19c55af49
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2019-04-03 12:04:06 +0200

    Pendiente de Terminar

---
+++ src/boletin_calculadora/Boletin_Calculadora.java
@@ -0,0 +1,17 @@
+
+
+package boletin_calculadora;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin_Calculadora {
+
+ 
+    public static void main(String[] args) {
+       Calculadora calc1 = new Calculadora();
+       calc1.setVisible(true);
+    }
+
+}
